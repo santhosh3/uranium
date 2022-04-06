ac76286fd58b59bf8b527ea5a0f84f6187a43c4a
santhosh <santhoshchinna109@gmail.com>
2022-04-06 19:47:05 +0530

    first commit ever

--- src/routes/route.js
+++ src/routes/route.js
@@ -3,7 +3,7 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/test-me', function (req, res) {
-    res.send('My first ever api!')
+    res.send('My first ever api! functionUp');
 });
 
 module.exports = router;
