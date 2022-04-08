2361468ff275d0c38a68d7b6ef367890c6f9cad8
santhosh <santhoshchinna109@gmail.com>
2022-04-08 09:32:08 +0530

    I done with my assignment

--- src/routes/route.js
+++ src/routes/route.js
@@ -1,17 +1,87 @@
 const express = require('express');
+const res = require('express/lib/response');
 const logger = require('./logger')
 
 const router = express.Router();
 
-router.get('/test-me', function (req, res) {
-    console.log('------------------')
-    console.log(req)
-    console.log('------------------')
-    console.log('These are the request query parameters: ', req.query)
-    res.send('My first ever api!')
+
+// router.get('/api/courses', function (req, res) {
+//     res.send([1,2,3]);
+// });
+
+// router.get('/api/posts/:year/:month', function (req,res){
+//     res.send(req.params);
+// });
+
+
+router.get('/movies', function(req,res){
+    let movies = ['rand be basnasti', 'RRR','kaleja', 'pokiri', 'maya'];
+    res.send(movies);
 });
 
 
+// router.get('/movies/:indexNumber',function(req,res){
+
+//         let movies = ['RRR', 'khiladi', 'ravi', 'sai'];
+//         let id = req.params.indexNumber;
+//         if(id < movies.length)
+//         {
+//             res.send(movies[id-1]);
+//         }
+
+
+// router.get('/movies/:indexNumber',function(req,res){
+
+//     let movies = ['RRR', 'khiladi', 'ravi', 'sai'];
+//     let id = req.params.indexNumber;
+//     if(id < movies.length)
+//     {
+//         res.send(movies[id-1]);
+//     }
+//     else
+//     {
+//         res.send('there is no movie name with respective id')
+//     }
+// });
+
+// router.get('/films', function(req,res){
+//     let films = 
+//     [{
+//         "id": 1,
+//         "name": "RRR"
+//     },
+//     {
+//         "id": 2,
+//         "name": "egga"
+//     },
+//     {
+//         "id": 3,
+//         "name": "Finding nemo"
+//     }];
+//     res.send(films);
+// });
+
+// router.get('/films/:filmid', function(req,res){
+//         let films = 
+//         [{
+//             "id": 1,
+//             "name": "RRR"
+//         },
+//         {
+//             "id": 2,
+//             "name": "egga"
+//         },
+//         {
+//             "id": 3,
+//             "name": "Finding nemo"
+//         }];
+//         let id = req.params.filmid;
+//         if(id === films.id)
+//         {
+//             res.send(films.name)
+//         }
+
+//     });
 
 
 module.exports = router;
